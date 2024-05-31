3d506b59b270548256e8ac5fb445d2610d18ca2d
SubodhJondhale <subodhjondhale1998@gmail.com>
2024-05-31 21:10:18 +0530

    TitlePreviewController

---
+++ Netflix Clone/ViewModels/TitlePreviewViewModel.swift
@@ -0,0 +1,14 @@
+//
+//  TitlePreviewViewModel.swift
+//  Netflix Clone
+//
+//  Created by Subodh Jondhale on 21/05/2024.
+//
+
+import Foundation
+
+struct TitlePreviewViewModel {
+    let title: String
+    let youtubeView: VideoElement
+    let titleOverview: String
+}
